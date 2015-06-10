29d3e3c4fb6147f4e392fd42f627b431b5ed0459
Arek Rypulak <arekrypulak@o2.pl>
2015-06-10 17:39:31 +0200

    arypulak - commit 2

---
+++ src/aarypulak/Tools/Polekwadratu.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace aarypulak\Tools;
+
+class Polekwadratu
+{
+
+    private $a;
+
+    public function setA($a)
+    {
+        $this->a = $a;
+
+        return $this;
+    }
+
+   
+
+    public function getA()
+    {
+        return $this->a;
+    }
+    
+    public function polekwadratu()
+    {
+        return $this->a * $this->a;
+    }
+}
